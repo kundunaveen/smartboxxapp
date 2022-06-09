f16114872c8a8bed1157a111b4e557fa8b4439fa
Harveer Singh <harveersingh@virtualemployee.com>
2022-06-09 21:17:05 +0530

    search issue fix

--- front-end/src/components/pages/Users.vue
+++ front-end/src/components/pages/Users.vue
@@ -210,7 +210,7 @@ export default {
     },
     clear() {
       this.loading = true;
-        $this.search="",
+        this.search="",
       axios
         .get("/api/users")
         .then((res) => {
