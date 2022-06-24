4f58301f48214ae5a74d6441368479f3070c0df1
Harveer Singh <harveersingh@virtualemployee.com>
2022-06-24 15:49:34 +0530

    fix loago issue

--- front-end/src/components/layout/Nav.vue
+++ front-end/src/components/layout/Nav.vue
@@ -14,13 +14,13 @@
           <a class="logo" href="#">
             <b>
               <img
-                v-bind:src="'../plugins/images/logo.png'"
+                v-bind:src="'http://65.2.143.196/anadev/smartboxxapp/front-end/dist/plugins/images/logo.png'"
                 alt="home"
               />
             </b>
             <span>
               <img
-                v-bind:src="'../plugins/images/logo-text.png'"
+                v-bind:src="'http://65.2.143.196/anadev/smartboxxapp/front-end/dist/plugins/images/logo-text.png'"
                 alt="homepage"
                 class="dark-logo"
               />
