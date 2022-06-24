4140eac0f328ce5e50fc7ad29950a488b2773c9a
Rahul kumar <rahulkumar9@virtualemployee.com>
2022-06-24 17:46:49 +0530

    icone change

--- front-end/src/components/pages/Profile.vue
+++ front-end/src/components/pages/Profile.vue
@@ -10,14 +10,20 @@
         <div class="row">
           <div class="col-md-12">
             <div class="panel panel-info">
-              <div class="panel-heading">Edit User</div>
+              <div class="panel-heading">Edit User <router-link
+                        type="reset"
+                        to="/dashboard"
+                        class="btn btn-default cancel-bttnn back-new-bttn"
+                      >
+                        <i class="fa fa-chevron-left" aria-hidden="true"></i> Back
+                      </router-link></div>
               <div class="panel-wrapper collapse in" aria-expanded="true">
                 <div class="panel-body">
                   <form action="#" @submit.prevent="updateUser">
                     <div class="form-body">
-                      <h3 class="box-title">Edit User</h3>
+                      <!-- h3 class="box-title">Edit User</h3-->
                       <p v-if="error" class="text-danger">{{ error }}</p>
-                      <hr />
+                      <!-- hr /-->
                       <div class="row">
                         <div class="col-md-6">
                           <div class="form-group">
@@ -190,7 +196,8 @@
                         to="/dashboard"
                         class="btn btn-default"
                       >
-                        Cancel
+                        <i class="fa fa-times" aria-hidden="true"></i>
+ Cancel
                       </router-link>
                     </div>
                   </form>
